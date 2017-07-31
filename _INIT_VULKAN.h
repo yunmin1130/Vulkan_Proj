68112dc5021539e96095c7fcfe386a5381c9fad2
chris9390 <chris9390@naver.com>
2017-07-31 16:12:19 +0900

    Add files via upload

---
+++ _INIT_VULKAN.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Renderer.h"
+//#include "Platform.h"
+#include <vector>
+//class Window;
+//#include "Window.h"
+
+
+
+
+class _INIT_VULKAN
+{
+public:
+	_INIT_VULKAN();
+	~_INIT_VULKAN();
+
+	void _INIT();
+	void _DEINIT();
+
+};
+
