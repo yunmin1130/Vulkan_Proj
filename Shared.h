6ac6753c4137d60d989243265b62a5b834a46443
yunmin1130 <yunmin1992@sogang.ac.kr>
2017-07-31 16:19:52 +0900

    Delete Shared.h

--- Shared.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Platform.h"
-#include <assert.h>
-#include <iostream>
-
-void ErrorCheck(VkResult result);
-
-uint32_t FindMemoryTypeIndex(const VkPhysicalDeviceMemoryProperties *gpu_memory_properties, const VkMemoryRequirements *memory_requirements, const VkMemoryPropertyFlags memory_properties);
